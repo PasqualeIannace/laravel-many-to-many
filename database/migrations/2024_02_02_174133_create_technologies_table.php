920771366e1c75269ae6af9fd908f51ded77c226
PasqualeIannace <pasquale.iannace@gmail.com>
2024-02-02 19:04:37 +0100

    add: Technology -rms

--- database/migrations/2024_02_02_174133_create_technologies_table.php
+++ database/migrations/2024_02_02_174133_create_technologies_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('types', function (Blueprint $table) {
+        Schema::create('technologies', function (Blueprint $table) {
             $table->id();
-            $table->string("platform");
+            $table->string("language");
+            $table->string("image");
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('types');
+        Schema::dropIfExists('technologies');
     }
 };
